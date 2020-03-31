182a65a7a8cf124330cbae8ccf34cb8ffa04d3b2
Vogusov <vogusov@gmail.com>
2020-03-31 22:01:26 +0300

    get users data

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-var token = 'YOUR INSTAGRAM TOKEN',
-    num_photos = 10, // maximum 20
-    container = document.getElementById( 'rudr_instafeed' ), // it is our <ul id="rudr_instafeed">
-    scrElement = document.createElement( 'script' );
- 
-window.mishaProcessResult = function( data ) {
-	for( x in data.data ){
-		container.innerHTML += '<li><img src="' + data.data[x].images.low_resolution.url + '"></li>';
-	}
+const API = 'https://raw.githubusercontent.com/Vogusov/users_API/master/',
+      user_ids = [101, 102, 103, 104, 105];
+
+let users_data = [];
+
+
+
+async function getResourse(url) {
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+  }
+  return await res.json();
 }
- 
-scrElement.setAttribute( 'src', 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + token + '&count=' + num_photos + '&callback=mishaProcessResult' );
-document.body.appendChild( scrElement );
+
+
+
+function getUsersData(arr) {
+  arr.forEach((id) => {
+    getResourse(API + `user_${id}.json`)
+      .then(data => {
+          console.dir(data);
+          users_data.push(data);
+        })  
+      .catch(err => console.log(err))
+  })
+}
+
+
+
+function init() {
+
+  getUsersData(user_ids);
+  
+  
+
+}
+
