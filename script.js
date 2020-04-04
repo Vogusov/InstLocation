b185cc14bef1822e95151b2c5381b44f9b62bc90
Vogusov <vogusov@gmail.com>
2020-04-04 19:16:41 +0300

    get result wo rendering

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const API = 'https://raw.githubusercontent.com/Vogusov/users_API/master/',
-  user_ids = [101, 102, 103, 104, 105];
+      user_ids = [101, 102, 103, 104, 105],
+      button = document.getElementById('button'),
+      result = document.getElementById('result');
 
 let users = [];
+let allUniqueUsersLocations = [];
 
 
 
@@ -26,17 +29,19 @@ function getUsersData(arr) {
           user.all_locations.push(post.location);
         })
         return user;
-      })
+      })      
     //делаем массив с уникальными локациями каждого пользователя:
       .then(user => {
-        user.unique_locations = [... new Set(user.all_locations)] ;
+        user.unique_locations = [... new Set(user.all_locations)];
         return user;
       })
     //Собираем массив со всеми пользователями:
       .then(user => {
         console.log(`data of user ${i}: `, user);
         users.push(user);
+        return users;
       })
+    
       .catch(err => console.log(err))
   })
   return users;
@@ -44,13 +49,58 @@ function getUsersData(arr) {
 
 
 
+
+ function getAllUniqueUsersLocations() {
+
+  users.forEach(user => {
+    allUniqueUsersLocations.push(user.unique_locations);
+  });
+  allUniqueUsersLocations = [... new Set( [].concat(...allUniqueUsersLocations)) ];
+  console.log('allUniqueUsersLocations: ', allUniqueUsersLocations);
+  return allUniqueUsersLocations;
+  
+}
+
+
+
+function getResultList() {
+  let resultList = new Object();
+
+  allUniqueUsersLocations.forEach(loc => {
+    resultList[loc] = [];
+
+    users.forEach(user => {
+      user.unique_locations.forEach(uniqueLoc => {
+        if (uniqueLoc === loc) {
+          resultList[loc].push(user.user_name);
+        }
+      });        
+    });
+  });
+
+  console.log('resultList', resultList);
+}
+
+
+
+function doIt() {
+
+  getAllUniqueUsersLocations();
+
+  getResultList();
+
+  
+
+  
+}
+
+
+
 // init
 function init() {
 
+  button.addEventListener('click', doIt)
+
   getUsersData(user_ids);
   console.log('1');
-
-
-
-
 }
